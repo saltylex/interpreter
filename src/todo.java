f19d534d0f47e9ffa472344bab4d2821d7ccf8c7
Lexi <68502135+saltylex@users.noreply.github.com>
2024-08-16 16:10:07 +0300

    add files

--- src/todo.java
+++
@@ -1,9 +0,0 @@
-public class todo {
-}
-/* TODO:
-    1. modify all string builders in statements.
-*   2. heap, file table and out are all shared therefore ensure synchronization
-*   3. scanner in the controller step by step might be fucked.
-*
-    4. JAVAFX IN PULA MEA
-*/
